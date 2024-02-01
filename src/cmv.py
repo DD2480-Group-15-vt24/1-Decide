adedf218a6f8f5f9d930f13e0d816c271a2215dc
SihamShahoud <113520260+SihamShahoud@users.noreply.github.com>
2024-02-01 23:34:42 +0100

    cmv.py
    Added LIC_11 to cmv.py

--- src/cmv.py
+++ src/cmv.py
@@ -173,8 +173,19 @@ class CMV:
                 self.cmv[10] = True
                 break
 
+    # CMV_condition_11
     def LIC_11(self):
-        return None
+        """There exists at least one set of two data points, (X[i],Y[i]) and (X[j],Y[j]), separated by
+        exactly G PTS consecutive intervening points, such that X[j] - X[i] < 0. (where i < j )
+        The condition is not met when NUMPOINTS < 3.
+        1 ≤ G PTS ≤ (NUMPOINTS - 2)
+        """
+        if Input.NUMPOINTS < 3 or Input.Parameters.G_PTS < 1 or Input.Parameters.G_PTS > (Input.NUMPOINTS - 2):
+            return
+        for i in range(Input.NUMPOINTS - Input.Parameters.G_PTS - 1):
+            if Input.POINTS[i+Input.Parameters.G_PTS+1, 0] - Input.POINTS[i, 0] < 0:
+                self.cmv[11] = True
+                break
     
     def LIC_12(self):
         return None
