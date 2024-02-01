b0f40a946489ae87796e04d11f98aee524d8dd88
Love Lindgren <lolindgr@kth.se>
2024-02-01 19:29:42 +0100

    Added unit tests for LIC_1

---
+++ tests/test_cmv.py
@@ -0,0 +1,63 @@
+import sys
+sys.path.append('../src')
+
+from cmv import CMV
+from data_structures import Input
+import numpy as np
+
+class test_CMV:
+
+    def test_LIC_1_valid(self):
+        """Test the LIC_1 function with simple example data to generate a valid result"""
+        test = CMV(np.zeros(15, dtype=bool))
+
+        Input.NUMPOINTS = 3
+        Input.POINTS = np.array([[1,1], [2,1], [2,2]])
+        Input.Parameters.RADIUS1 = 0.5
+        
+        CMV.LIC_1(test)
+        assert test.cmv[1], 'Condition for test_LIC_1_valid is False'
+
+    def test_LIC_1_invalid(self):
+        """Test the LIC_1 function with simple example data to generate an invalid result"""
+        test = CMV(np.zeros(15, dtype=bool))
+
+        Input.NUMPOINTS = 3
+        Input.POINTS = np.array([[1,1], [2,1], [2,2]])
+        Input.Parameters.RADIUS1 = 1
+        
+        CMV.LIC_1(test)
+        assert not test.cmv[1], 'Condition for test_LIC_1_invalid is True'
+
+    def test_LIC_4_valid(self):
+        return
+
+    def test_LIC_4_invalid(self):
+        return
+
+
+    def test_LIC_7_valid(self):
+        return
+
+    def test_LIC_7_invalid(self):
+        return
+
+
+    def test_LIC_10_valid(self):
+        return
+
+    def test_LIC_10_invalid(self):
+        return
+
+
+    def test_LIC_13_valid(self):
+        return
+
+    def test_LIC_13_invalid(self):
+        return
+
+if __name__ == "__main__":
+    instance = test_CMV()
+
+    instance.test_LIC_1_valid()
+    instance.test_LIC_1_invalid()
