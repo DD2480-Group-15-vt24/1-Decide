faa683f9944f38a58df98e769237b20cf23030ab
SihamShahoud <113520260+SihamShahoud@users.noreply.github.com>
2024-02-01 20:43:34 +0100

    calc_Angel.py

--- calc_Angel.py
+++ calc_Angel.py
@@ -1,6 +1,3 @@
-
-
-
 def calcAngle(self, p1, v, p2):
         """
         calcAngle method calculates the angle between three points (p1,v,p2) 
@@ -8,9 +5,9 @@ def calcAngle(self, p1, v, p2):
         calcAngle uses the calc_distance method to calculate the distances between points,
         and then it computes the angle using the law of cosines.
         """
-        d1 = self.calc_distance(self, p1, v)
-        d2 = self.calc_distance(self, p2, v)
-        d3 = self.calc_distance(self, p1, p2)
+        d1 = Units.calc_distance(self, p1, v)
+        d2 = Units.calc_distance(self, p2, v)
+        d3 = Units.calc_distance(self, p1, p2)
 
         # Check for undefined angle
         if d1 == 0 or d2 == 0:
