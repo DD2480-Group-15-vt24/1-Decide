001e1958b05544cd0d847fc636578666908b3591
SihamShahoud <113520260+SihamShahoud@users.noreply.github.com>
2024-02-01 17:04:38 +0100

    Update cmv_condtion_14.py
    Added enhancment to the solution of condition LIC_14.

--- cmv_condtion_14.py
+++ cmv_condtion_14.py
@@ -1,43 +1,26 @@
-import numpy as np
+#CMV_condition_14
+def LIC_14(self):
+        """There exists at least one set of three data points, separated by exactly E_PTS and F_PTS consecutive
+        intervening points, respectively, that are the vertices of a triangle with area greater
+        than AREA1. In addition, there exist three data points (which can be the same or different
+        from the three data points just mentioned) separated by exactly E_PTS and F_PTS consecutive
+        intervening points, respectively, that are the vertices of a triangle with area less than
+        AREA2. Both parts must be true for the LIC to be true. The condition is not met when
+        NUMPOINTS < 5.
+        0 ≤ AREA2
+        """
+        if Input.NUMPOINTS < 5 or Input.Parameters.E_PTS < 1 or Input.Parameters.F_PTS < 1 or Input.Parameters.AREA2 < 0:
+            return
 
-# Global declaration
-POINTS = []    
-NUMPOINTS = 0  
-E_PTS = 0      
-F_PTS = 0      
-AREA1 = 0.0    
-AREA2 = 0.0    
+        # Check the first part of the condition (greater than AREA1)
+        for i in range(Input.NUMPOINTS - Input.Parameters.E_PTS - Input.Parameters.F_PTS - 2):
+            g_area= Utils.calc_triangle_area(self, Input.POINTS[i], Input.POINTS[i+Input.Parameters.E_PTS+1], Input.POINTS[i+Input.Parameters.E_PTS+Input.Parameters.F_PTS+2])
 
-# CMV condition_14
-def lic_14(POINTS, E_PTS, F_PTS, AREA1, AREA2, NUMPOINTS):
-    if NUMPOINTS < 5 or AREA2 < 0:
-        return False  # Not enough points or invalid AREA2, condition not met
-    
-    # Iterate through triangles with area greater than AREA1
-    for i in range(NUMPOINTS - E_PTS - F_PTS - 2):
-        x1, y1 = POINTS[i]              #Coordinates for first point
-        x2, y2 = POINTS[i + E_PTS + 1]  #Coordinates for second point
-        x3, y3 = POINTS[i + E_PTS + F_PTS + 2]  #Coordinates for third point
+            if g_area > Input.Parameters.AREA1:
+                # Check the second part of the condition (less than AREA2)
+                for j in range(i + Input.Parameters.E_PTS + Input.Parameters.F_PTS + 2, Input.NUMPOINTS - Input.Parameters.E_PTS - Input.Parameters.F_PTS - 2):
+                    l_area = Utils.calc_triangle_area(self, Input.POINTS[j], Input.POINTS[j+Input.Parameters.E_PTS+1], Input.POINTS[j+Input.Parameters.E_PTS+Input.Parameters.F_PTS+2])
 
-        # Calculate the area of the first triangle
-        area = t_area(x1, y1, x2, y2, x3, y3)
-
-        # Check if the area is greater than AREA1
-        if area > AREA1:
-            # Iterate through triangles with area less than AREA2
-            for j in range(i + E_PTS + F_PTS + 2, NUMPOINTS - E_PTS - F_PTS - 1):
-                x4, y4 = POINTS[j]
-                x5, y5 = POINTS[j + E_PTS + 1]
-                x6, y6 = POINTS[j + E_PTS + F_PTS + 2]
-
-                # Calculate the area of the second triangle
-                area2 = t_area(x4, y4, x5, y5, x6, y6)
-
-                # Check if the area is less than AREA2
-                if area2 < AREA2:
-                    return True  
-            return False  # No triangles meeting the conditions found
-
-def t_area(x1, y1, x2, y2, x3, y3):
-    # Calculate the area of a triangle formed by three points.
-    return 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+                    if l_area < Input.Parameters.AREA2:
+                        self.cmv[14] = True
+                        return
