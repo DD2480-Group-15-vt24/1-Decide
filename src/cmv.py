c4bca2fa73114b1d088333a02241f4980ed23d80
SihamShahoud <113520260+SihamShahoud@users.noreply.github.com>
2024-02-01 22:19:57 +0100

    cmv.py
    Added LIC_5 to main branch

--- src/cmv.py
+++ src/cmv.py
@@ -75,8 +75,18 @@ class CMV:
     def LIC_4(self):
         return None
     
+    #LIC_5
     def LIC_5(self):
-        return None
+        """There exists at least one set of two consecutive data points, (X[i],Y[i]) and (X[j],Y[j]),
+        such that X[j] - X[i] < 0. (where i = j-1)
+        """
+        if Input.NUMPOINTS < 2:
+            return
+
+        for i in range(Input.NUMPOINTS - 1):
+            if Input.POINTS[i+1, 0] - Input.POINTS[i, 0] < 0:
+                self.cmv[5] = True
+                break
     
     def LIC_6(self):
         return None
