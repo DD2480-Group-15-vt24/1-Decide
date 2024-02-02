931f0d2bfdcc924f9a2df248511ece0c1f0fa423
selmaozdere <89855155+selmaozdere@users.noreply.github.com>
2024-02-02 13:09:08 +0100

    fix: re-added accidentally deleted functions

--- tests/test_cmv.py
+++ tests/test_cmv.py
@@ -261,13 +261,27 @@ def test_LIC_11_invalid():
     return
 
 
-def test_LIC_12_valid():
-    return
+def test_LIC_12_valid(self):
+    Input.NUMPOINTS = 5
+    Input.POINTS = np.array([[0, 0], [1, 1], [2, 2], [3, 1], [4, 0]])
+    Input.Parameters.LENGTH1 = 2.0
+    Input.Parameters.LENGTH2 = 0.5
+    Input.Parameters.K_PTS = 1
 
+    test = CMV(np.zeros(15, dtype=bool))
+    CMV.LIC_12(test)
+    assert test.cmv[12], 'Condition for test_LIC_12_valid is False'
 
-def test_LIC_12_invalid():
-    return
+def test_LIC_12_invalid(self):
+    Input.NUMPOINTS = 5
+    Input.POINTS = np.array([[0, 0], [1, 1], [2, 2], [3, 1], [4, 0]])
+    Input.Parameters.LENGTH1 = 0.1
+    Input.Parameters.LENGTH2 = 0.5
+    Input.Parameters.K_PTS = 1
 
+    test = CMV(np.zeros(15, dtype=bool))
+    CMV.LIC_12(test)
+    assert not test.cmv[12], 'Condition for test_LIC_12_invalid is True'
 
 def test_LIC_13_valid():
     """Test the LIC_10 function with simple example data to generate a valid result"""
