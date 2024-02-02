1ee887a2241df4bc529bc576d84df315f0be937e
sihamshahoud <siham.shahoud1991@gmail.com>
2024-02-02 17:25:32 +0100

    FX:test for lic_14 invalid

--- tests/test_cmv.py
+++ tests/test_cmv.py
@@ -198,6 +198,7 @@ def test_LIC_7_invalid():
     assert not test.cmv[7], "Condition for test_LIC_7_invalid is True"
 
 
+
 def test_LIC_9_valid():
     Input.NUMPOINTS = 6
     Input.POINTS = np.array([[0, 0], [1, 1], [2, 2], [3, 1], [4, 0], [5, 1]])
@@ -316,3 +317,19 @@ def test_LIC_13_invalid():
     test = CMV(np.zeros(15, dtype=bool))
     CMV.LIC_13(test)
     assert not test.cmv[13], "Condition (1) and (2) for test_LIC_13_invalid is True"
+
+def test_LIC_14_invalid():
+    Input.NUMPOINTS = 5
+    Input.POINTS = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]])
+    Input.Parameters.E_PTS = 1
+    Input.Parameters.F_PTS = 1
+    Input.Parameters.AREA1 = 1.0
+    Input.Parameters.AREA2 = 0.5
+    result = CMV(np.zeros(15, dtype=bool))
+    CMV.LIC_14(result)
+    assert not result.cmv[14], 'Condition for Test Case 1 is False'
+
+
+
+
+
